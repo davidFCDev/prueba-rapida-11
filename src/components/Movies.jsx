f858c2912021a6790fe3324bd672d170cc45d734
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-26 23:43:51 +0200

    mapeo peliculas api

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,10 +2,10 @@ function ListOfMovies({ movies }) {
   return (
     <ul className="movies">
       {movies.map((movie) => (
-        <li className="movie" key={movie.imdbID}>
-          <h2>{movie.Title}</h2>
-          <p>{movie.Year}</p>
-          <img src={movie.Poster} alt={movie.Title} />
+        <li className="movie" key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.title} />
         </li>
       ))}
     </ul>
